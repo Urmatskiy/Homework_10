3d7722bd6843e140eaeb5aa30634b2506ea71160
Urmatskiy <nickiton14@gmail.com>
2023-01-26 18:47:34 +0300

    Написал программу

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array1 = {"54555", "Hello", "YeS", "No", "1"};
+Console.WriteLine($"[{string.Join(", ", array1)}]");
+
+string[] array2 = new string[array1.Length];
+
+
+int count = 0;
+for (int i = 0; i < array1.Length; i++)
+{
+    if (array1[i].Length <= 3)
+    {
+        array2[count] = array1[i];
+        count++;
+    }  
+}
+
+Console.Write($"{string.Join(" ", array2)}");
